8a771b74ee01d959404776359215e42153b1b801
Mohammed Salim <renablehots@gmail.com>
2024-08-28 16:47:47 +0400

    Updated test
    - Changed test to show the new version

--- tests/slifer.test.ts
+++ tests/slifer.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from 'bun:test';
 import Slifer from '..';
 
 test("Slifer Version", () => {
-    expect(Slifer.getVersion()).toBe("Slifer v0.0.1");
+    expect(Slifer.getVersion()).toBe("Slifer v0.0.2");
 })
