b705ebcc620285298de110f39adb6891e5ecd698
Mohammed Salim <renablehots@gmail.com>
2024-08-28 20:15:53 +0400

    Changed how font loading works

--- src/modules/graphics.ts
+++ src/modules/graphics.ts
@@ -6,12 +6,9 @@ class Graphics {
     private renderer!: Pointer;
 
     loadFont(path: string, pt: number) : Font {
-        const ft = libttf.symbols.TTF_OpenFont(Buffer.from(path+"\x00"), pt);
-        if (ft == null) {
-            throw `Font failed to load`;
-        }
-
-        return ft;
+        const font = libttf.symbols.TTF_OpenFont(Buffer.from(path+"\x00"), pt);
+        if (font == null) throw `Font load failed`;
+        return font;
     }
 
     flip() : void {
