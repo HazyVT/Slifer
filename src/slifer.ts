9e72e3ea243578c0e0f0961920523dff2d89b3c9
Mohammed Salim <renablehots@gmail.com>
2025-01-24 15:25:45 +0400

    Removed rectangle class from slifer file

--- src/slifer.ts
+++ src/slifer.ts
@@ -173,23 +173,3 @@ export class SliferClass {
     }
 }
 
-export class Rectangle {
-    private readonly pointer;
-    readonly x;
-    readonly y;
-    readonly width;
-    readonly height;
-
-    constructor(x: number, y: number, width: number, height: number) {
-        const arr = new Uint32Array(4);
-        arr[0] = x;
-        arr[1] = y;
-        arr[2] = width;
-        arr[3] = height;
-        this.pointer = ptr(arr);
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-}
