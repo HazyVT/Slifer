c75003fa9ec5e6b83644623957ebb19bc3050760
Mohammed Salim <renablehots@gmail.com>
2025-01-24 15:54:16 +0400

    Updated rectangle class to use vectors

--- src/engine/rectangle.ts
+++ src/engine/rectangle.ts
@@ -1,23 +1,24 @@
 import { ptr } from "bun:ffi";
+import Vector2 from "./vector";
 
 class Rectangle {
     private readonly pointer;
-    public x;
-    public y;
-    public width;
-    public height;
+    public position: Vector2;
+    public size: Vector2;
 
-    constructor(x: number, y: number, width: number, height: number) {
+    constructor(position: Vector2, size: Vector2) {
         const arr = new Uint32Array(4);
-        arr[0] = x;
-        arr[1] = y;
-        arr[2] = width;
-        arr[3] = height;
+        arr[0] = position.x;
+        arr[1] = position.y;
+        arr[2] = size.x;
+        arr[3] = size.y;
         this.pointer = ptr(arr);
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        this.position = position;
+        this.size = size;
+    }
+
+    static empty() {
+        return new Rectangle(new Vector2(0, 0), new Vector2(0, 0));
     }
 }
 
